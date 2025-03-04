6579783b25d3ab0e619207be7207ff7975b99c66
Sievlinh <sievlinhk@gmail.com>
2025-03-04 22:13:58 +0700

    Add files via upload

---
+++ Exercise 5/src/data.js
@@ -0,0 +1,102 @@
+/* Your data here */
+import Doremon from './assets/Doremon.jpg'
+import Jerry from './assets/Jerry.jpg'
+import Oggy from './assets/Oggy.jpg'
+import Olaf from './assets/Olaf.jpg'
+import Pikachu from './assets/Pikachu.jpg'
+import Shinchan from './assets/Shinchan-Crayon.jpg'
+import Spongebob from './assets/Spongebob.jpg'
+import Pooh from './assets/Pooh.jpg'
+import Grizzly from './assets/Grizzly.jpg'
+import Panda from './assets/Panda.jpg'
+import Icebear from './assets/Icebear.jpg'
+export const CARDS = [
+    {
+        title: "Doremon",
+        from: "Doremon",
+        image: {
+            src: Doremon,
+            alt: 'Doremon',
+        },
+    },
+    {
+        title: "Jerry",
+        from: "Tom&Jerry",
+        image: {
+            src: Jerry,
+            alt: 'Jerry',
+        },
+    },
+    {
+        title: "Oggy",
+        from: "Oggy and The cockroaches",
+        image: {
+            src: Oggy,
+            alt: 'Oggy',
+        },
+    },
+    {
+        title: "Olaf",
+        from: "Frozen",
+        image: {
+            src: Olaf,
+            alt: 'Olaf',
+        },
+    },
+    {
+        title: "Pikachu",
+        from: "Pokemons",
+        image: {
+            src: Pikachu,
+            alt: 'Pikachu',
+        },
+    },
+    {
+        title: "Shinchan-Crayon",
+        from: "Shinchan-Crayon",
+        image: {
+            src: Shinchan,
+            alt: 'Shinchan',
+        },
+    },
+    {
+        title: "Spongebob",
+        from: "The SpongeBob SquarePants",
+        image: {
+            src: Spongebob,
+            alt: 'Spongebob',
+        },
+    },
+    {
+        title: "Pooh",
+        from: "Winnie the Pooh",
+        image: {
+            src: Pooh,
+            alt: 'Pooh',
+        },
+    },
+    {
+        title: "Grizzly",
+        from: "We bare bears",
+        image: {
+            src: Grizzly,
+            alt: 'Grizzly ',
+        },
+    },
+    {
+        title: "Panda",
+        from: "We bare bears",
+        image: {
+            src: Panda,
+            alt: 'Panda ',
+        },
+    },
+    {
+        title: "Ice Bare",
+        from: "We bare bears",
+        image: {
+            src: Icebear,
+            alt: 'Icebare',
+        },
+    }
+]
